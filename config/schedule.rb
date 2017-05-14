f7a087beda5af2e0578f2cfbf0ef8dbd047c83f6
Paokin <zzz@zzz.com>
2017-05-14 20:06:34 +0300

    add version display in version edit

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,9 +5,9 @@ set :environment, :development
 ruby_path = "/home/andrey/.rbenv/bin/rbenv exec"
 job_type :rake, "cd :path && RAILS_ENV=:environment #{ruby_path} bundle exec rake :task :output"
 
-#every 2.hours do
- # rake "db:backup"
-#end
+every 2.hours do
+  rake "db:backup"
+end
 every 1.day do
 	rake "pack:expired"
    
