5b0552bda1759aa24ecdf49a9d6a1ff22702896d
Andrey Paokin <andrejpaokin@yandex.ru>
2018-07-06 18:52:29 +0300

    core 600

--- engines/core/app/controllers/core/application_controller.rb
+++ engines/core/app/controllers/core/application_controller.rb
@@ -2,7 +2,7 @@ module Core
   class ApplicationController < ActionController::Base
     layout "layouts/application"
 
-    before_filter :require_login,:set_current_userz
+    before_filter :require_login,:set_current_user
 
     rescue_from MayMay::Unauthorized, with: :not_authorized
 
