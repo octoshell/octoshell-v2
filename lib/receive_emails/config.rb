fcebe9540189d72ee40e3ee1b33560ce421ba7e2
Andrey Paokin <andrejpaokin@yandex.ru>
2018-10-05 14:44:27 +0300

    mend

--- lib/receive_emails/config.rb
+++ lib/receive_emails/config.rb
@@ -1,6 +1,6 @@
 module ReceiveEmails
   CONFIG = {
-    socket_path: 'mail.sock',
+    socket_path: '/home/andrey/mail.sock',
     socket_permissions: 066,
     ticket_creation_allowed?: true
   }.freeze
