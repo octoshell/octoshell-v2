8af0983b1efa33df31af54943cb72368cfd36493
Sergey Zhumatiy <serg@parallel.ru>
2018-10-17 11:34:10 +0300

    wip

--- engines/jobstat/db/migrate/20180921111132_create_jobstat_job_mail_filters.rb
+++ engines/jobstat/db/migrate/20180921111132_create_jobstat_job_mail_filters.rb
@@ -1,5 +1,6 @@
 class CreateJobstatJobMailFilters < ActiveRecord::Migration
   def change
+    return if File.exists? '/tmp/skip_bad_migrations.txt'
     create_table :jobstat_job_mail_filters do |t|
       t.string :condition
       t.integer :user_id
