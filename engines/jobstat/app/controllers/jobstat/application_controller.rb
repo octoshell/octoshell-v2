e49b30eb136208572d4541ecd736a39343b66ce9
Sergey Zhumatiy <serg@parallel.ru>
2019-01-28 09:01:29 +0300

    feat(jobstat): add actuall clusters and partitions to jobstat comtroller

--- engines/jobstat/app/controllers/jobstat/application_controller.rb
+++ engines/jobstat/app/controllers/jobstat/application_controller.rb
@@ -27,11 +27,10 @@ module Jobstat
 
       @PER_PAGE = 100
 
-      lom1 = ClusterConfig.new("Ломоносов-1", "lomonosov-1")
-      lom2 = ClusterConfig.new("Ломоносов-2", "lomonosov-2")
+      # lom1 = ClusterConfig.new("Ломоносов-1", "lomonosov-1")
+      # lom2 = ClusterConfig.new("Ломоносов-2", "lomonosov-2")
 
-      @clusters = {"lomonosov-1" => lom1, "lomonosov-2" => lom2}
-
+      # @clusters = {"lomonosov-1" => lom1, "lomonosov-2" => lom2}
       slurm_states = {
         "RUNNING" => "Running",
         "COMPLETED" => "Completed",
@@ -41,31 +40,44 @@ module Jobstat
         "NODE_FAIL" => "Node failed",
       }
 
-      lom1.states = slurm_states
-      lom2.states = slurm_states
-
-      lom1.partitions = {
-        "regular4" => {"cores" => 8, "gpus" => 0},
-        "regular6" => {"cores" => 12, "gpus" => 0},
-        "hdd4" => {"cores" => 8, "gpus" => 0},
-        "hdd6" => {"cores" => 12, "gpus" => 0},
-        "test" => {"cores" => 8, "gpus" => 0},
-        "gpu" => {"cores" => 8, "gpus" => 2},
-        "gputest" => {"cores" => 8, "gpus" => 2},
+      @clusters = {}
+      Core::Cluster.where(available_for_work: true).each{|c|
+        name=c.name_ru
+        if name.nil? || name==''
+          name=c.name_en
+        end
+        @clusters[name]=ClusterConfig.new(c.name, c.description)
+        @clusters[name].states=slurm_states
       }
 
-      lom2.partitions = {
-        "compute" => {"cores" => 14, "gpus" => 1},
-        "low_io" => {"cores" => 14, "gpus" => 1},
-        "compute_prio" => {"cores" => 14, "gpus" => 1},
-        "test" => {"cores" => 14, "gpus" => 1},
-        "pascal" => {"cores" => 12, "gpus" => 2},
-      }
+      # lom1.states = slurm_states
+      # lom2.states = slurm_states
 
-      @default_cluster = "lomonosov-2"
+      # lom1.partitions = {
+      #   "regular4" => {"cores" => 8, "gpus" => 0},
+      #   "regular6" => {"cores" => 12, "gpus" => 0},
+      #   "hdd4" => {"cores" => 8, "gpus" => 0},
+      #   "hdd6" => {"cores" => 12, "gpus" => 0},
+      #   "test" => {"cores" => 8, "gpus" => 0},
+      #   "gpu" => {"cores" => 8, "gpus" => 2},
+      #   "gputest" => {"cores" => 8, "gpus" => 2},
+      # }
+
+      # lom2.partitions = {
+      #   "compute" => {"cores" => 14, "gpus" => 1},
+      #   "low_io" => {"cores" => 14, "gpus" => 1},
+      #   "compute_prio" => {"cores" => 14, "gpus" => 1},
+      #   "test" => {"cores" => 14, "gpus" => 1},
+      #   "pascal" => {"cores" => 12, "gpus" => 2},
+      # }
+
+      @default_cluster = Core::Cluster.last.description #"lomonosov-2"
 
       @states_options = slurm_states.keys
-      @partitions_options = lom1.partitions.keys + lom2.partitions.keys
+      @partitions_options = Core::Cluster.where(available_for_work: true).map{|c|
+        c.partitions.map{|p| p.name}
+      }.flatten.uniq
+      #lom1.partitions.keys + lom2.partitions.keys
 
     end
 
