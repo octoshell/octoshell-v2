0607e9c38d35a2833e14feff447ca9ae0b72f95d
Sergey Zhumatiy <serg@parallel.ru>
2019-03-25 13:15:03 +0300

    fix(pack): fix error with CSRF protection

--- engines/pack/app/controllers/pack/application_controller.rb
+++ engines/pack/app/controllers/pack/application_controller.rb
@@ -5,6 +5,8 @@ module Pack
 #    #layout 'layouts/pack/application'
     layout "layouts/application"
 
+ 	skip_before_action :verify_authenticity_token
+ 	
     before_action do |controller|
     	@extra_css="pack/pack.css"
     end
