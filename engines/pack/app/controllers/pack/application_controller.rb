4f668a05954cbadef9fdea65a69c97039027d660
Sergey Zhumatiy <serg@parallel.ru>
2019-07-19 13:08:56 +0300

    fix(pack): restore default layout

--- engines/pack/app/controllers/pack/application_controller.rb
+++ engines/pack/app/controllers/pack/application_controller.rb
@@ -2,12 +2,12 @@ module Pack
   class ApplicationController < ::ApplicationController
     #ActionController::Base
 #    include AuthMayMay
-#    #layout 'layouts/pack/application'
-    layout "layouts/application"
+    layout 'layouts/pack/application'
+    #layout "layouts/application"
     skip_before_action :verify_authenticity_token
 
     before_action do |controller|
-    	@extra_css="pack/pack.css"
+      @extra_css=["pack/pack.css"]
     end
 
   end
