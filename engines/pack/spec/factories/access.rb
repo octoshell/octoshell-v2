52f14175ec5f49842a3b5a5dab151874226f2b8e
sdenis1995 <sdenis1995@gmail.com>
2019-07-31 12:35:47 +0300

    Merge branch 'master' into detailed -> sden-changes

--- engines/pack/spec/factories/access.rb
+++ engines/pack/spec/factories/access.rb
@@ -1,8 +1,4 @@
 FactoryBot.define do
-  factory :access, class: "Pack::Access" do
-    association :to, factory: :version, strategy: :create
-    association :who, factory: :user, strategy: :create
-    association :created_by, factory: :user, strategy: :create
-    end_lic { (Date.current + 30).to_s }
+
   end
 end
