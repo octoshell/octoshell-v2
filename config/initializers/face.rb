4016f27fdd9e0f8e0b300b8526e87535e81bca63
Andrey <andrejpaokin@yandex.ru>
2019-08-07 18:25:46 +0300

    fix(all): errors in production

---
+++ config/initializers/face.rb
@@ -0,0 +1,12 @@
+module Face
+  extend Octoface
+  octo_configure do
+
+  end
+
+
+  # menu.add_item(Face::MenuItem.new({name: t("admin_submenu.projects"),
+  #                                   url: core.admin_projects_path,
+  #                                   regexp: /core\/admin\/projects/})) if  :manage, :projects
+
+end
