baa56798b5bc3db4ca451589c1b753e23db2edcb
SDenis <sdenis1995@gmail.com>
2020-03-25 20:43:27 +0300

    merge

--- db/migrate/20190320081952_add_initiator_to_jobstat_jobs.jobstat.rb
+++ db/migrate/20190320081952_add_initiator_to_jobstat_jobs.jobstat.rb
@@ -1,5 +1,5 @@
 # This migration comes from jobstat (originally 20190320081454)
-class AddInitiatorToJobstatJobs < ActiveRecord::Migration
+class AddInitiatorToJobstatJobs < ActiveRecord::Migration[4.2]
   def change
     add_reference :jobstat_jobs, :initiator
   end
