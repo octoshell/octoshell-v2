75153efa9e49b4a0fd6e9c808901d5bda0ab6100
Sergey Zhumatiy <serg@parallel.ru>
2020-03-31 13:13:55 +0300

    refactor notifications

--- engines/core/app/models/core/organization.rb
+++ engines/core/app/models/core/organization.rb
@@ -4,7 +4,7 @@
 #
 #  id           :integer          not null, primary key
 #  abbreviation :string(255)
-#  checked      :boolean          default(FALSE)
+#  checked      :boolean          default("false")
 #  name         :string(255)
 #  created_at   :datetime
 #  updated_at   :datetime
