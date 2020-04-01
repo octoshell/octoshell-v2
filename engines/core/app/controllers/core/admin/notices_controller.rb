c9f40faf84fd0c467f7d918c0a7850c6a0b92552
Sergey Zhumatiy <serg@parallel.ru>
2020-04-01 16:37:20 +0300

    add notices code

---
+++ engines/core/app/controllers/core/admin/notices_controller.rb
@@ -0,0 +1,86 @@
+module Core
+  class Admin::NoticesController < Admin::ApplicationController
+    def index
+      respond_to do |format|
+        format.html do
+          @search = Notice.search(params[:q])
+          search_result = @search.result(distinct: true).order(:id)
+          @notices = search_result
+          without_pagination :notices
+        end
+        format.json do
+          @notices = Notice.search(params[:q])
+          json = { records: @notices.page(params[:page]).per(params[:per]), total: @notices.count }
+          render json: json
+        end
+
+      end
+    end
+
+    def new
+      @notice = Notice.new
+      @notice.sourceable_id = params[:sourceable_id]
+      @notice.sourceable_type = params[:sourceable_type]
+      @notice.linkable_id = params[:linkable_id] if params[:linkable_id]
+      @notice.linkable_type = params[:linkable_type] if params[:linkable_type]
+      @notice.category = params[:category].to_i
+      @notice.message = params[:message]
+      @notice.count = params[:count].to_i
+    end
+
+    def create
+      @notice = Notice.new
+      @notice.sourceable_id = params[:sourceable_id]
+      @notice.sourceable_type = params[:sourceable_type]
+      @notice.linkable_id = params[:linkable_id] if params[:linkable_id]
+      @notice.linkable_type = params[:linkable_type] if params[:linkable_type]
+      @notice.category = params[:category].to_i
+      @notice.message = params[:message]
+      @notice.count = params[:count].to_i
+      if @notice.save
+        redirect_to [:admin, @notice]
+      else
+        render :new
+      end
+    end
+
+    def show
+      @notice = Notice.find(params[:id])
+    end
+
+    def edit
+      @notice = Notice.find(params[:id])
+    end
+
+    def update
+      @notice = Notice.find(params[:id])
+      if @notice.update notice_params
+        redirect_to [:admin, @notice]
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @notice = Notice.find_by_id(params[:id])
+      if @notice
+        @notice.destroy
+        if params[:retpath]
+          redirect_to params[:retpath]
+        else
+          redirect_to [:admin, Notice]
+        end
+      end
+    end
+
+    private
+
+    def notice_params
+      params.require(:notice).permit(
+        :sourceable_id, :sourceable_type,
+        :linkable_id, :linkable_type,
+        :type, :message, :count, :retpath
+        )
+    end
+  end
+end
