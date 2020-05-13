96e28bbf05e31d83513efdc90c540200971c99f4
Sergey Zhumatiy <serg@parallel.ru>
2020-05-13 18:07:38 +0300

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
+ActiveRecord::Schema.define(version: 2020_05_13_144834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
