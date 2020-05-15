c131b36db1da8e95de39128d0e82cfc4af327475
Sergey Zhumatiy <serg@parallel.ru>
2020-05-15 13:10:52 +0300

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_05_13_144834) do
+ActiveRecord::Schema.define(version: 2020_05_13_152800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "announcement_recipients", id: :serial, force: :cascade do |t|
     t.integer "user_id"
@@ -339,13 +359,13 @@ ActiveRecord::Schema.define(version: 2020_05_13_144834) do
   end
 
   create_table "core_notice_show_options", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "notice_id"
+    t.bigint "core_user_id"
+    t.bigint "core_notice_id"
     t.boolean "hidden", default: false, null: false
     t.boolean "resolved", default: false, null: false
     t.string "answer"
-    t.index ["notice_id"], name: "index_core_notice_show_options_on_notice_id"
-    t.index ["user_id"], name: "index_core_notice_show_options_on_user_id"
+    t.index ["core_notice_id"], name: "index_core_notice_show_options_on_core_notice_id"
+    t.index ["core_user_id"], name: "index_core_notice_show_options_on_core_user_id"
   end
 
   create_table "core_notices", id: :serial, force: :cascade do |t|
