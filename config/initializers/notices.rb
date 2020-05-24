ebe0cf9d96cda7d443e1f0b2cf9021a27e30b1be
Sergey Zhumatiy <serg@parallel.ru>
2020-05-24 14:36:05 +0300

    (fix)[all] merge bot support

--- config/initializers/notices.rb
+++ config/initializers/notices.rb
@@ -7,15 +7,15 @@ module Core
   end
 end
 
-Face::MyMenu.items_for(:admin_submenu) do
-  if can?(:manage, :tickets)
-    add_item('notices', t("notices"), core.admin_notices_path,
-             %r{^notices})
-  end
-end
-
-Core::Notice.register_def_per_user_handler
-
-Core::Notice.register_kind 'jobstat' do |notice, user, params, request|
-  nil
-end
+#Face::MyMenu.items_for(:admin_submenu) do
+#  if can?(:manage, :tickets)
+#    add_item('notices', t("notices"), core.admin_notices_path,
+#             %r{^notices})
+#  end
+#end
+#
+#Core::Notice.register_def_per_user_handler
+#
+#Core::Notice.register_kind 'jobstat' do |notice, user, params, request|
+#  nil
+#end
--- engines/pack/spec/factories/access.rb
+++ engines/pack/spec/factories/access.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
-
+  factory :access, class: "Pack::Access" do
+    association :to, factory: :version, strategy: :create
+    association :who, factory: :user, strategy: :create
+    association :created_by, factory: :user, strategy: :create
+    end_lic { (Date.current + 30).to_s }
   end
 end
