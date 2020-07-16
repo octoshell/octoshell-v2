f5c6357ce89db4418a2f05b6248e771eff8ee5b4
Sergey Zhumatiy <serg@parallel.ru>
2020-07-16 08:57:42 +0300

    wip: user control (witewide) works

--- engines/core/app/models/core/notice.rb
+++ engines/core/app/models/core/notice.rb
@@ -34,64 +34,98 @@ module Core
     OTHER = -1
     # message:  text    = notice text
     # count:    integer = notices count if applicable
-    # category: integer = 0 for wide notices, >0 for per-user notices, other - undefined
+    # category: integer = 0 for wide notices,
+    #                     >0 for per-user notices,
     #                     1 = simple per-user notification, 2+ = call handler...
+    #                     other - undefined
     # kind:     string  = for handler selection
     # show_from: date   = when start to show (nil=now)
     # show_till: date   = when stop to show  (nil=forever)
     # active:   integer = 1(or nil) = is active now (for use in handler only)
-    
-    def visible?
-      opt = self.notice_show_options.take
-      ! opt&.hidden
+
+    def visible?(user = nil)
+      # user ||= current_user
+      opt = notice_show_options.where(user: user).take
+      !opt&.hidden
     end
 
-    def self.register_kind k, &block
+    def self.register_kind(k, &block)
       @notice_handlers ||= {}
       @notice_handlers[k] = block
       logger.warn "::: @handlers= #{@notice_handlers.inspect}"
     end
 
-    def self.handle notice, user, params, request
-      # logger.warn "||||| #{@notice_handlers.inspect}"
-      return nil if ! @notice_handlers
+    def self.handle(notice, user, params, request)
+      mylog "||||| #{@notice_handlers.inspect}"
+      return nil unless @notice_handlers
 
       handler = @notice_handlers[notice.kind.to_s]
       # nil / [:flash_type, message]
-      logger.warn "-++> #{handler}"
+      mylog "-++> #{handler}"
       handler.nil? ? nil : handler.call(notice, user, params, request)
     end
 
     def source_name
-      sourceable.nil? ? '----' :
-      sourceable.respond_to?(:full_name) ?
-        sourceable.full_name :
-        sourceable.respond_to?(:title_ru) ?
-          sourceable.title_ru : sourceable.name_ru
+      if sourceable.nil?
+        '----'
+      elsif sourceable.respond_to?(:full_name)
+        sourceable.full_name
+      elsif sourceable.respond_to?(:title_ru)
+        sourceable.title_ru
+      else
+        sourceable.name_ru
+      end
     end
 
-    # 
-    def self.register_def_per_user_handler
-      Core::Notice.register_kind nil.to_s do |notice, user, params, request|
+    def self.register_def_nil_kind_handler
+      Core::Notice.register_kind nil.to_s do |notice, user, _, request|
         # logger.warn "-> #{notice.id}/#{notice.kind}/#{notice.category}"
-        [
-          :info,
-          "#{notice.message} #{notice.add_close(request)}".html_safe
-        ]
-      end 
+        if notice.visible? user
+          [
+            :info,
+            "#{notice.message} #{notice.add_close(request)}".html_safe
+          ]
+        else
+          nil
+        end
+      end
     end
+
+    # def self.register_def_worldwide_handler
+    #   Core::Notice.register_kind nil.to_s do |notice, user, params, request|
+    #     # logger.warn "-> #{notice.id}/#{notice.kind}/#{notice.category}"
+    #     [
+    #       :info,
+    #       "#{notice.message} #{notice.add_close(request)}".html_safe
+    #     ]
+    #   end
+    # end
 
     include Rails.application.routes.url_helpers
     include ActionView::Helpers::UrlHelper
 
-    def add_close request
-      #FIXME!!!! [:admin, :core, self] does not work :(((((
-      link = link_to '<i class="fas fa-bell-slash close"></i>'.html_safe, "/core/admin/notices/#{id}/hide?retpath=#{request.fullpath}", data:{retpath: request.fullpath}
+    def add_close(request)
+      # FIXME!!!! [:admin, :core, self] does not work :(((((
+      link = link_to(
+        '<i class="fas fa-bell-slash close"></i>'.html_safe,
+        "/core/admin/notices/#{id}/hide?retpath=#{request.fullpath}",
+        data: { retpath: request.fullpath }
+      )
       link
     end
 
     def self.mylog(txt)
       File.open('mylog.txt', 'a+') { |f| f.write "#{txt}\n" }
+    end
+
+    def category_str
+      if category.zero?
+        I18n.t('.core.admin.notices.site-wide')
+      elsif category == 1
+        I18n.t('.core.admin.notices.per-user')
+      else
+        I18n.t('.core.admin.notices.other')
+      end
     end
 
     def self.show_notices(current_user, params, request)
@@ -121,7 +155,7 @@ module Core
         .each do |note|
 
         user_option = note.notice_show_options.where(user: current_user).take
-        mylog "SiteWide ================== #{user_option.inspect}; #{note.inspect}"
+        mylog "SiteWide =============== #{user_option.inspect}; #{note.inspect}"
         next if user_option&.hidden
 
         data << conditional_show_notice(note, current_user, params, request)
@@ -132,10 +166,10 @@ module Core
     def self.conditional_show_notice(note, current_user, params, request)
       mylog "cond1 #{note.inspect}"
       n = Time.current
-      return if note.show_from && (note.show_from > n)
-      return if note.show_till && (note.show_till < n)
+      return nil if note.show_from && (note.show_from > n)
+      return nil if note.show_till && (note.show_till < n)
       ret = Core::Notice.handle(note, current_user, params, request)
-      mylog "cond2 - result=#{ret}"
+      mylog "cond2 - result=#{ret.inspect}"
       ret
     end
   end
