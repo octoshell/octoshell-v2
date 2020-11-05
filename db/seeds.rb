a126116fd3d20d55014542d767ed5bdbb4669e9b
Andrey <andrejpaokin@yandex.ru>
2020-11-05 17:26:28 +0300

    fix(core): fix translations of countries and cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-  ActiveRecord::Base.connection.tables.each do |table|
-    next if table == 'schema_migrations'
-    ActiveRecord::Base.connection.execute("TRUNCATE #{table} CASCADE")
-  end
+  # ActiveRecord::Base.connection.tables.each do |table|
+  #   next if table == 'schema_migrations'
+  #   ActiveRecord::Base.connection.execute("TRUNCATE #{table} CASCADE")
+  # end
 
 
   puts 'Running seeds'
