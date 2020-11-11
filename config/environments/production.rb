32fc1e8972af7211587da03b326651f4637c1d41
Sergey Zhumatiy <serg@parallel.ru>
2020-11-11 15:50:12 +0300

    fix(base): force to not use ssl for smtp connections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "localhost",
+    :tls => false,
+    :enable_starttls_auto => false,
+    :ssl => false,
     :port => 25
   }
 
