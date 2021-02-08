378963eb1551a9a0c64b54524db4f650d21ec02a
Sergey Zhumatiy <serg@parallel.ru>
2021-02-08 17:46:57 +0300

    fix(support): ticket search_by_content

--- docker/docker_init.sh
+++ docker/docker_init.sh
@@ -1,18 +1,18 @@
-nohup redis-server &
+nohup ./redis-server &
 
-service postgresql start
+#service postgresql start
 
-sudo -u postgres psql -c "CREATE USER octo WITH PASSWORD 'octopass';"
-sudo -u postgres psql -c "ALTER USER octo CREATEDB;"
+#sudo -u postgres psql -c "CREATE USER octo WITH PASSWORD 'octopass';"
+#sudo -u postgres psql -c "ALTER USER octo CREATEDB;"
 
-cd /root/octoshell-v2
+cd /octoshell-v2-build
 
 source ~/.bashrc
 
-bundle exec rake db:create
+#bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rake db:seed
+#bundle exec rake db:seed
 bundle exec rake assets:precompile
 
-bash dev
+bash ./dev
 
