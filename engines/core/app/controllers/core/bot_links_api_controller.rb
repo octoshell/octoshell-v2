c7667368e7ee1c86b8f8c747ca98754b16ddfd46
Evgeny Shulgin <izaronplatz@gmail.com>
2021-04-07 14:47:09 +0300

    Add user jobs info

--- engines/core/app/controllers/core/bot_links_api_controller.rb
+++ engines/core/app/controllers/core/bot_links_api_controller.rb
@@ -5,20 +5,21 @@ module Core
     def index
       result = params
 
-      if params[:method] == 'auth'
+      case params[:method]
+      when 'auth'
         result = BotLinksApiHelper.auth(params)
-      elsif params[:method] == 'topics'
+      when 'topics'
         result = BotLinksApiHelper.topics(params)
-      elsif params[:method] == 'clusters'
+      when 'clusters'
         result = BotLinksApiHelper.clusters(params)
-      elsif params[:method] == 'user_projects'
+      when 'user_projects'
         result = BotLinksApiHelper.user_projects(params)
-      elsif params[:method] == 'user_tickets'
+      when 'user_tickets'
         result = BotLinksApiHelper.user_tickets(params)
-      elsif params[:method] == 'create_ticket'
+      when 'user_jobs'
+        result = BotLinksApiHelper.user_jobs(params)
+      when 'create_ticket'
         result = BotLinksApiHelper.create_ticket(params)
-      elsif params[:method] == 'notify'
-        BotLinksApiHelper.notify(params)
       end
 
       render json: result
