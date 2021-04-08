3eaa1aaf5f90b7246fafba24f9dda96b8dfa44f7
Zhumatiy Sergey <serg@parallel.ru>
2021-04-08 11:32:37 +0300

    Update bot_links_api_controller.rb

--- engines/core/app/controllers/core/bot_links_api_controller.rb
+++ engines/core/app/controllers/core/bot_links_api_controller.rb
@@ -5,23 +5,9 @@ module Core
     def index
       result = params
 
-      case params[:method]
-      when 'auth'
-        result = BotLinksApiHelper.auth(params)
-      when 'topics'
-        result = BotLinksApiHelper.topics(params)
-      when 'clusters'
-        result = BotLinksApiHelper.clusters(params)
-      when 'user_projects'
-        result = BotLinksApiHelper.user_projects(params)
-      when 'user_tickets'
-        result = BotLinksApiHelper.user_tickets(params)
-      when 'user_jobs'
-        result = BotLinksApiHelper.user_jobs(params)
-      when 'create_ticket'
-        result = BotLinksApiHelper.create_ticket(params)
+      if %q[auth topics clusters user_projects user_tickets user_jobs create_ticket].include? params[:method]
+        result = BotLinksApiHelper.call(params[:method], params)
       end
-
       render json: result
     end
   end
