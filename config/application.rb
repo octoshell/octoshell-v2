bd270008e0d2991cbbe7ebf7b8e4b76e2766dceb
apaokin <andrejpaokin@yandex.ru>
2022-01-19 13:54:51 +0300

    fix(all): new Gemfile.lock for honeybadger (#284)

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
+require 'uri'
 require_relative 'boot'
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
