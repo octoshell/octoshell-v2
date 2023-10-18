480050878f2881f757202948e436ad537e632db2
apaokin <andrejpaokin@yandex.ru>
2023-10-18 12:31:43 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,10 +111,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "localhost",
-    :tls => false,
-    :enable_starttls_auto => false,
-    :ssl => false,
+    :address => "smtp.parallel.ru",
     :port => 25
   }
 
