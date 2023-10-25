88139c2bddc62929612f18c93508bac0cec382fe
apaokin <andrejpaokin@yandex.ru>
2023-10-25 19:44:55 +0300

    fix(support): work-arround search column absence

---
+++ db/migrate/20231025160413_add_search_to_support_fields.support.rb
@@ -0,0 +1,8 @@
+# This migration comes from support (originally 20231025131955)
+class AddSearchToSupportFields < ActiveRecord::Migration[5.2]
+  def change
+    unless column_exists? :support_fields, :search
+      add_column :support_fields, :search, :boolean, default: false
+    end
+  end
+end
