5b4c45a56747d7ce24b62a21f5562aa3d45ad728
apaokin <andrejpaokin@yandex.ru>
2023-10-25 19:43:57 +0300

    fix(docker): add correct config for sidekiq

--- docker/dev-sidekiq.sh
+++ docker/dev-sidekiq.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bundle exec sidekiq -q announcements_mailer -q pack_worker -q auth_mailer -q core_mailer -q sessions_mailer -q sessions_validator -q sessions_starter -q stats_collector -q support_mailer -q synchronizer -q cloud_computing_task_worker -q perf_queue
+bundle exec sidekiq -C config/sidekiq.yml
