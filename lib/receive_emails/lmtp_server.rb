daae4156b68f0601d85d143a09f377f8ca1ba23a
apaokin <andrejpaokin@yandex.ru>
2023-11-27 19:52:17 +0300

    fix(base): return support of lmtp server

--- lib/receive_emails/lmtp_server.rb
+++ lib/receive_emails/lmtp_server.rb
@@ -1,16 +1,20 @@
-# require 'receive_emails/config'
-# require 'receive_emails/email_processor'
-# require 'lmtp'
-# require 'zip'
-#
-# module ReceiveEmails
-#   def self.lmtp_server
-#     ::LmtpServer.new(CONFIG[:socket_path], CONFIG[:socket_permissions]) do |msg|
-#       EmailProccessor.new(msg).process
-#     end
-#   end
-#   unless Rails.env.test?
-#     puts '== Starting =='
-#     lmtp_server.start
-#   end
-# end
+require 'receive_emails/email_processor'
+require 'lmtp'
+require 'zip'
+
+module ReceiveEmails
+  CONFIG = {
+    socket_path: 'mail.sock',
+    socket_permissions: 066
+  }
+  def self.lmtp_server
+    ::LmtpServer.new(CONFIG[:socket_path], CONFIG[:socket_permissions]) do |msg|
+      EmailProccessor.new(msg).process
+    end
+  end
+
+  def self.start_server
+    puts '== Starting =='
+    lmtp_server.start
+  end
+end
