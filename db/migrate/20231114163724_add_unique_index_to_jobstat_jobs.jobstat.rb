f3a0d6e7b055d070d279181ea53c3cae160b36fc
Avalenor <kingofamber2003@gmail.com>
2024-07-10 15:49:26 +0300

    changesnew

--- db/migrate/20231114163724_add_unique_index_to_jobstat_jobs.jobstat.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from jobstat (originally 20231114163340)
-class AddUniqueIndexToJobstatJobs < ActiveRecord::Migration[5.2]
-  def change
-   # add_index :jobstat_jobs, %i[cluster drms_task_id drms_job_id], unique: true,
-                                                                   #name: 'uniq_jobs'
-  end
-end
