483e8441f98c183d6638a2b844f542f13cc8a7a3
Avalenor <kingofamber2003@gmail.com>
2024-12-27 17:38:50 +0300

    1

--- engines/octoface/lib/engines/main_app.rb
+++ engines/octoface/lib/engines/main_app.rb
@@ -1,12 +1,14 @@
 module FakeMainApp
   extend Octoface
   octo_configure :main_app do
-    # add('User') # теперь во всех модулях доступен report_class
-    add_ability(:manage, :users, 'superadmins') #Если абилки нет, то она создаётся
-    add_controller_ability(:manage, :users, 'admin/users') #В указанных контроллерах используется :manage :reports
-    add_ability(:manage, :groups, 'superadmins') #Если абилки нет, то она создаётся
-    add_controller_ability(:manage, :groups, 'admin/groups') #В указанных контроллерах используется :manage :reports
-    add_ability(:manage, :options, 'superadmins') #Если абилки нет, то она создаётся
+    add_ability(:manage, :users, 'superadmins')
+    add_controller_ability(:manage, :users, 'admin/users')
+    add_ability(:manage, :groups, 'superadmins')
+    add_controller_ability(:manage, :groups, 'admin/groups')
+    add_ability(:manage, :options, 'superadmins')
+    add_controller_ability(:manage, :policies, 'admin/policies') # Для Policies
+    add_ability(:manage, :policies, 'superadmins') # Права для Policies
+
     after_init do
       Face::MyMenu.items_for(:user_submenu) do
         add_item('profile', t('user_submenu.profile'), main_app.profile_path, 'profiles',
@@ -21,6 +23,9 @@ module FakeMainApp
         add_item_if_may('groups', t('admin_submenu.groups'),
                         main_app.admin_groups_path,
                         'admin/groups')
+
+        add_item_if_may('policies', t('admin_submenu.policies'),
+                        main_app.policies_path, 'admin/policies') # Пункт для Policies
 
         if User.superadmins.include? current_user
           add_item('sidekiq', t("admin_submenu.sidekiq"), main_app.admin_sidekiq_web_path)
