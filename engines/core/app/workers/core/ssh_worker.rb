4f93fcc0b764067e79442d8ee4ec4b54252e8cce
apaokin <andrejpaokin@yandex.ru>
2026-02-24 14:12:15 +0300

    fix(core): SshWorker

--- engines/core/app/workers/core/ssh_worker.rb
+++ engines/core/app/workers/core/ssh_worker.rb
@@ -3,10 +3,10 @@ module Core
     include Sidekiq::Worker
     sidekiq_options queue: :default
 
-    def perform(template, args)
+    def perform(template, args = [])
       case template
       when 'calculate_resources'
-        Core::ResourceControl.send(template, *args)
+        Core::ResourceControl.send(template)
       when 'sync_with_cluster'
         Core::QueueAccess.send(template, *args)
       else
