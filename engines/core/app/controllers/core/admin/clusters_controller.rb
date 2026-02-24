ef654dd9d68c9a649c4bbf6ba19fee69c5abf6b0
apaokin <andrejpaokin@yandex.ru>
2026-02-24 14:29:11 +0300

    feat(core): edit submitted and running in partitions

--- engines/core/app/controllers/core/admin/clusters_controller.rb
+++ engines/core/app/controllers/core/admin/clusters_controller.rb
@@ -2,16 +2,18 @@ module Core
   class Admin::ClustersController < Admin::ApplicationController
     before_action :octo_authorize!
     def index
-      #@clusters = Cluster.order(:id)
-      q=params[:q]
-      if q
-        @clusters = Cluster.finder(params[:q])
-      else
-        @clusters = Cluster.order(:id)
-      end
+      # @clusters = Cluster.order(:id)
+      q = params[:q]
+      @clusters = if q
+                    Cluster.finder(params[:q])
+                  else
+                    Cluster.order(:id)
+                  end
       respond_to do |format|
         format.html
-        format.json {render json: { records: @clusters.page(params[:page]).per(params[:per]), total: @clusters.count }}
+        format.json do
+          render json: { records: @clusters.page(params[:page]).per(params[:per]), total: @clusters.count }
+        end
       end
     end
 
@@ -30,7 +32,7 @@ module Core
     def create
       @cluster = Cluster.new(cluster_params)
       if @cluster.save
-        redirect_to [:admin, @cluster], notice: t("flash.cluster_created")
+        redirect_to [:admin, @cluster], notice: t('flash.cluster_created')
       else
         render :new
       end
@@ -43,7 +45,7 @@ module Core
     def update
       @cluster = Cluster.find(params[:id])
       if @cluster.update(cluster_params)
-        redirect_to [:admin, @cluster], notice: t("flash.cluster_updated")
+        redirect_to [:admin, @cluster], notice: t('flash.cluster_updated')
       else
         render :edit
       end
@@ -62,10 +64,10 @@ module Core
       params.require(:cluster).permit(:name, :host, :admin_login,
                                       :available_for_work, :description,
                                       *Core::Cluster.locale_columns(:name),
-                                      partitions_attributes: [:id, :_destroy,
-                                                              :name, :resources],
-                                      quotas_attributes: [:id, :quota_kind_id,
-                                                          :value, :_destroy])
+                                      partitions_attributes: %i[id _destroy
+                                                                name resources max_submitted_jobs max_running_jobs],
+                                      quotas_attributes: %i[id quota_kind_id
+                                                            value _destroy])
     end
   end
 end
